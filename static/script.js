1e2238f8c6e3a43f85a1b0cb2b81cbe6a074cec3
davidesartori <davide.sartori.10@gmail.com>
2022-05-21 17:57:11 +0200

    esito registrazione aggiunto

--- static/script.js
+++ static/script.js
@@ -11,7 +11,19 @@ function registration() {
   .then(function(data) { // Here you get the data to modify as you please
     var result = data.success;
 
-    document.write(result);
+    document.getElementById("form").innerHTML = "";
+    document.getElementById("titolo").innerHTML = "Esito registrazione";
+
+    var outcome;
+
+    if(result == true) {
+      outcome = "Registrazione effettuata con successo";
+    }
+    else {
+      outcome = "Username già esistente";
+    }
+
+    document.getElementById("risultato").innerHTML = outcome;
 
     return;
     }).catch( error => console.error(error));
