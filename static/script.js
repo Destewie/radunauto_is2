72461d2529e533011c40d4e3c51e75ea7eb37c8d
Stella Bosio <stella.bosio@studenti.unitn.it>
2022-05-22 11:38:35 +0200

    Aggiunta pagina creazione club

--- static/script.js
+++ static/script.js
@@ -28,3 +28,34 @@ function registration() {
     return;
     }).catch( error => console.error(error));
 };
+
+function create_club() {
+  var name = document.getElementById('name').value;
+  var owner = document.getElementById('owner').value;
+
+  fetch('../api/clubs', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify( { name: name, owner: owner } ),
+    }).then((resp) => resp.json())
+
+  .then(function(data) { // Here you get the data to modify as you please
+    var result = data.success;
+
+    document.getElementById("form").innerHTML = "";
+    document.getElementById("titolo").innerHTML = "Esito creazione club";
+
+    var outcome;
+
+    if(result == true) {
+      outcome = "Club creato con successo";
+    }
+    else {
+      outcome = "Errore";
+    }
+
+    document.getElementById("risultato").innerHTML = outcome;
+
+    return;
+    }).catch( error => console.error(error));
+};
