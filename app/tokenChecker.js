af3bb8a2ebecbac905f4e01d8133fd1b4c6e4894
davidesartori <davide.sartori.10@gmail.com>
2022-05-23 01:16:22 +0200

    bug fixing

--- app/tokenChecker.js
+++ app/tokenChecker.js
@@ -14,10 +14,7 @@ const tokenChecker = function(req, res, next) {
 	// decode token, verifies secret and checks exp
 	jwt.verify(token, process.env.SUPER_SECRET, function(err, decoded) {
 		if (err) {
-			return res.status(403).send({
-				success: false,
-				message: 'Failed to authenticate token.'
-			});
+			return res.redirect('/login.html');
 		} else {
 			// if everything is good, save to request for use in other routes
 			req.loggedUser = decoded;
