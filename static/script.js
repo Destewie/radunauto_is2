78bc160f84f0eab6b954348b598bbb3757502048
Des <federico.ds25@gmail.com>
2022-05-23 14:50:40 +0200

    aggiunto il logout e fatti ritocchi vari

--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
+//----------------------------------------------------------------------------
+
 function registration() {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
@@ -28,6 +30,8 @@ function registration() {
     return;
     }).catch( error => console.error(error));
 };
+
+//----------------------------------------------------------------------------
 
 function create_club() {
   var name = document.getElementById('name').value;
@@ -70,13 +74,65 @@ function login() {
     body: JSON.stringify( { username: username, password: password } ),
     }).then((resp) => resp.json())
 
-  .then(function(data) { // Here you get the data to modify as you please
+  .then(function(data) { 
+    // qui "data" è il json che è stato tornato da  authentication
+    //volendo qui puoi fare quello che vuoi con quel json
     if(data.success) {
-      result = data.token;
+      window.location = "/home.html"; //se il login è andato, rimando alla home
+    } else {
+      window.location = "login.html"; //se il login non è andato, rimando di nuovo alla pagina del login 
     }
-
-    window.location = "/home.html";
 
     return;
     }).catch( error => console.error(error));
 };
+
+//----------------------------------------------------------------------------
+
+//funzione presa dall'internet. La uso come black box
+function getCookie(name) {
+  var dc = document.cookie;
+  var prefix = name + "=";
+  var begin = dc.indexOf("; " + prefix);
+  if (begin == -1) {
+      begin = dc.indexOf(prefix);
+      if (begin != 0) return null;
+  }
+  else
+  {
+      begin += 2;
+      var end = document.cookie.indexOf(";", begin);
+      if (end == -1) {
+      end = dc.length;
+      }
+  }
+  // because unescape has been deprecated, replaced with decodeURI
+  //return unescape(dc.substring(begin + prefix.length, end));
+  return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+
+//----------------------------------------------------------------------------
+
+function logout() {
+  var cookie = getCookie("token");
+
+  fetch('../api/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify( { cookie : cookie } ), //inglobo il cookie preso nel body della richiesta post che faccio al server
+    }).then((resp) => resp.json())
+
+  .then(function(data) { 
+    // qui "data" è il json che è stato tornato da logout.js
+    //volendo qui puoi fare quello che vuoi con quel json
+    if(data.success) {
+      window.location = "/logout.html"; //se il login è andato, rimando alla home
+    } else {
+      window.location = "home.html"; //se il login non è andato, rimando di nuovo alla pagina del login 
+    }
+
+    return;
+    }).catch( error => console.error(error));
+};
+
+//----------------------------------------------------------------------------
