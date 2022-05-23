33b05bd4c33ee160a57a0e4dff1a68ab1d09dc82
davidesartori <davide.sartori.10@gmail.com>
2022-05-24 01:25:56 +0200

    bug fixing

--- static/script.js
+++ static/script.js
@@ -70,7 +70,7 @@ function create_event() {
   fetch('../api/raduni', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify( { title: title, description: description } ),
+    body: JSON.stringify( { title: title, description: description, club: club } ),
     }).then((resp) => resp.json())
 
   .then(function(data) {
